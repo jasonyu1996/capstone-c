4e4f68a6b0f300ddb1fd09baa7c5fb710274b371
Jason Yu <yuzhijingcheng1996@hotmail.com>
2022-04-06 18:03:49 +0800

    inner enclave can be entered

--- samples/nested.c
+++ samples/nested.c
@@ -24,9 +24,13 @@ TEECAP_ATTR_DEDICATED_STACK TEECAP_ATTR_HAS_METAPARAM void outer() {
     // struct enclave* encl = enclave_create(inner_code, crypto_data, 0, runtime);
     // encl = ecall(encl, 22);
     // enclave_destroy(encl, runtime);
-    void *sealed = runtime->sealed;
-    sealed = sealed_setup(sealed, inner_code, 0, 0, runtime);
-    direct_call(sealed);
+    void *sealed_ = runtime->sealed;
+    sealed_[TEECAP_SEALED_OFFSET_PC] = inner_code;
+    sealed_[TEECAP_SEALED_OFFSET_EPC] = 0;
+    sealed_[TEECAP_SEALED_OFFSET_DEDICATED_STACK] = 0; // stack is actually specified upon call
+    sealed_[TEECAP_SEALED_OFFSET_METAPARAM] = runtime;
+    seal(sealed_);
+    sealed_();
 
     // void* nested_code = runtime->runtime->malloc(128);
     // int i = 0;
@@ -47,23 +51,13 @@ TEECAP_ATTR_DEDICATED_STACK TEECAP_ATTR_HAS_METAPARAM void outer() {
     TEECAP_STACK_RETURN;
 }
 
-TEECAP_ATTR_DEDICATED_STACK TEECAP_ATTR_HAS_METAPARAM void inner() {
-    TEECAP_STACK_SETUP;
-
-    struct enclave_runtime* runtime = TEECAP_METAPARAM;
-    int call_code = runtime->shared[0];
-
-    print(call_code);
-    // print(22);
-
-    TEECAP_METAPARAM = runtime;
-
-    TEECAP_STACK_RETURN;
+TEECAP_ATTR_HAS_METAPARAM void inner() {
+     print(42);
 }
 
 int main(struct teecap_runtime* runtime) {
-    void* outer_code = runtime->malloc(128);
-    void* inner_code = runtime->malloc(128);
+    void* outer_code = runtime->malloc(256);
+    void* inner_code = runtime->malloc(256);
     int i = 0;
     void* crypto_base;
     TEECAP_BUILD_CP(crypto_base, 0);
